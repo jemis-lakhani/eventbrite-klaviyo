1a74248541e305c9a80035bbe5c259312d6e7946
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-06-18 02:46:27 +0530

    fix: route update

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,8 +6,8 @@ const { webhook,attendeeUpdated,
 
 const router = express.Router();
 router.post('/', webhook);
-router.post('/attendeeUpdated', attendeeUpdated);
-router.post('/orderUpdated', orderUpdated);
+// router.post('/attendeeUpdated', attendeeUpdated);
+// router.post('/orderUpdated', orderUpdated);
 // router.get('/avatars', controller.get);
 
 module.exports = router;
