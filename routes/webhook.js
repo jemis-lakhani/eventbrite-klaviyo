a4fd0d058fd918652ec90e1f94b0514aea7c4d59
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-06-19 14:28:28 +0530

    added test endpoint

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { webhook,attendeeUpdated,
   orderUpdated,
  } = require('../controllers/webhook');
+const hello = require('../controllers/hello');
 
 
 const router = express.Router();
 router.get('/', (req, res) => res.json({ message: 'hello' }));
+router.post('/hello', hello);
 router.post('/', webhook);
 // router.post('/attendeeUpdated', attendeeUpdated);
 // router.post('/orderUpdated', orderUpdated);
